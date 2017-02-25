77c6e42e57e646dcc704ac451117d12bbf657656
yusukew62 <yusuke.w62@gmail.com>
2017-02-25 18:25:13 +0900

    ex04/ex4-1.c

---
+++ ex04/ex4-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name is Yusuke Watanabe\n");
+    printf("Drivers license number is 1234567890\n");
+    printf("Birthday is yyyy.mm.dd\n");
+
+    return (0);
+}
